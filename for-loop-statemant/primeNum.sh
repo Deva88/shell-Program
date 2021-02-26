84903d077ac2840cfa63a60ed20963b3abf5e008
Deva88 <devendra.raj.sdm@gmail.com>
2021-02-26 09:10:33 +0530

    Added: Check prime number or Not

--- for-loop-statemant/primeNum.sh
+++ for-loop-statemant/primeNum.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-
-read n
+echo "Enter Number :"
+read  n
 count=1
-for ((i=2; i<=n; i++))
+for ((i=2 ; i<=n ; i++))
 do
 if [ $(($n % $i)) -eq 0 ]
 then
 count=$(($count + 1))
 fi
 done
-if [$count -eq 2 ]
-then 
-
+if [ $count -eq 2 ]
+then
 echo "is prime number"
 else
 echo " is not prime number"
