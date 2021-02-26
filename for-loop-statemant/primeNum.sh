65ded45a484e22fb342db10aa415abb4c7e2ba2b
Deva88 <devendra.raj.sdm@gmail.com>
2021-02-26 08:49:35 +0530

    Added: Count 1 to 5 number

---
+++ for-loop-statemant/primeNum.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read n
+count=1
+for ((i=2; i<=n; i++))
+do
+if [ $(($n % $i)) -eq 0 ]
+then
+count=$(($count + 1))
+fi
+done
+if [$count -eq 2 ]
+then 
+
+echo "is prime number"
+else
+echo " is not prime number"
+fi
