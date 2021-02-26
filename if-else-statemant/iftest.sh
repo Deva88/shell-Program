384dc099350bec11ff20b7b42b0803c7ef472547
Deva88 <devendra.raj.sdm@gmail.com>
2021-02-26 08:02:42 +0530

    Added: Number is Greater than or Equal to or Less than

--- if-else-statemant/iftest.sh
+++ if-else-statemant/iftest.sh
@@ -4,10 +4,7 @@ var1=10
 var2=10
 if [ $var2 -gt $var1 ]
 then
-echo "$var2 is greater then $var1"
-elif [ $var2 -eq $var1 ]
-then
-   echo "variables are equal"
+echo "$var2 is greater then or equal to $var1"
 else
-   echo "$var2 is less then $var1"
+echo "$var2 is less then $var1"
 fi
