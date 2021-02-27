6cdc4fa01ea809add639eb02eb87f6674a300995
Deva88 <devendra.raj.sdm@gmail.com>
2021-02-27 17:09:49 +0530

    Added: calulater Fahrenheit to Celsium and celsium to Fahreheit

--- function/degF_degC.sh
+++ function/degF_degC.sh
@@ -1,11 +1,17 @@
-#! /bin/bash
+#! /bin/bash -x
 
+function degree ()
+{
+degc=$1
+degf=$2
 echo "Fahrenheit to Celsius"
-read -p "Degree in Celsius= " degc
-degf=$(( (degc*9/5)+32 ))
+degf=$(( (degc-32)*5/9 ))
 echo "Faherenhiet =" $degf
 echo
 echo "Celsius to Fahrenheit"
-read -p "Degree in Fahreheit= " degf
-degc=$(( (degf-32)*5/9 ))
+degc=$(( (degf*9/5)+32 ))
 echo "Celsius =" $degc
+}
+read -p "Degree in Celsius= " degc
+read -p "Degree in Fahreheit= " degf
+degree $degc $degf
