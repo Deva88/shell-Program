68c34aa44e0b7629b018ebba20f8df3deebe871e
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-03 13:17:48 +0530

    Added: 5Random 2 Digit value than find sum and average

--- sequence-statement/randomSumAvg.sh
+++ sequence-statement/randomSumAvg.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
+#!/bin/bash -x
 
-a=$(((RANDOM%10) +10))
-b=$(((RANDOM%10) +10))
-c=$(((RANDOM%10) +10))
-d=$(((RANDOM%10) +10))
-e=$(((RANDOM%10) +10))
+#To Random number
+randomNum1=$(((RANDOM%90)+10))
+randomNum2=$(((RANDOM%90)+10))
+randomNum3=$(((RANDOM%90)+10))
+randomNum4=$(((RANDOM%90)+10))
+randomNum5=$(((RANDOM%90)+10))
 
-sum=$(($a+$b+$c+$d+$e))
-echo "sum of Random value = " $sum
+#To Print the Number
+echo "First Random Number =" $randomNum1
+echo "Second Random Number=" $randomNum2
+echo "third Random Number =" $randomNum3
+echo "fourt Random Number =" $randomNum4
+echo "fifth Random Number =" $randomNum5
 
-avg=$(($sum/5))
-echo "Average of Random value = " $avg
+#Add The Random value
+sum=$(( $randomNum1+$randomNum2+$randomNum3+$rand>
+echo "Sum of Random 5 Number =" $sum
+
+#To Random avarage
+avg=$(( sum/5 ))
+echo "Avrage of 5 Random Number =" $avg
+
