1c1a8e963b46fe6faab576d18d84d178554f11b7
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-04 11:50:27 +0530

    Added: 3 Number do following arithmetic Operationfind one is max. and min

--- if-elif-else-statemant/arithOper.sh
+++ if-elif-else-statemant/arithOper.sh
@@ -1,10 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "enter three numbers"
 
+#take User Input
 read a
 read b
 read c
 
+#calculate all Result
 result1=$(($a+$b*$c))
 echo $result1
 result2=$(($c+$a/$b))
@@ -13,3 +15,42 @@ result3=$(($a%$b+$c))
 echo $result3
 result4=$(($a*$b+$c))
 echo $result4
+
+#logic to check Maximum Result and Minimum Result
+if [ $result1 -gt  $result2 -a $result1 -gt $result3 -a $result1 -gt $result4 ]
+then
+maximum=$result1
+
+elif [ $result2 -gt $result3 -a $result2 -gt $result4 ]
+then
+maximum=$resut2
+
+elif [ $result3 -gt $result4 ]
+then
+maximum=$result3
+
+else
+maximum=$result4
+fi
+
+
+
+if [ $result1 -lt  $result2 -a $result1 -lt $result3 -a $result1 -lt $result4 ]
+then
+minimum=$result1
+
+elif [ $result2 -lt $result3 -a $result2 -lt $result4 ]
+then
+minimum=$result2
+
+elif [ $result3 -lt $result4 ]
+then
+minimum=$result3
+
+else
+minimum=$result4
+fi
+
+echo "Minimum :" $minimum
+echo "Maximum :" $maximum
+
